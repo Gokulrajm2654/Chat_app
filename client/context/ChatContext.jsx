0f5de104475a6bde64386566d28fefdc9056762d
Gokulrajm2654 <gokulrajm2021@gmail.com>
2025-06-11 13:05:09 +0530

    Complete chatcontainer , rightside bar

--- client/context/ChatContext.jsx
+++ client/context/ChatContext.jsx
@@ -94,7 +94,7 @@ export const ChatProvider = ({ children }) => {
         users,
         selectedUser,
         getUsers,
-        setMessages,
+        getMessages,
         sendMessage,
         setSelectedUser,
         unseenMessages,
