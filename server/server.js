c0dd095fa21706933c2fb791a3108b64dc18c7fa
Gokulrajm2654 <gokulrajm2021@gmail.com>
2025-06-11 19:26:08 +0530

    update cors and backend url

--- server/server.js
+++ server/server.js
@@ -37,7 +37,9 @@ io.on("connection", (socket) => {
 
 // Middleware setup
 app.use(express.json({ limit: "4mb" }));
-app.use(cors());
+app.use(cors({
+    origin: "https://chat-app-beta-six-64.vercel.app"
+}));
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter)
